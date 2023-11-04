75f70fb58ab7ccf462c3e0d276c3df41f16a8a1c
Assaf Izhar <assaf.izhar@gmail.com>
2023-11-04 14:34:38 -0400

    Create scenes.py

---
+++ scenes.py
@@ -0,0 +1,17 @@
+def scene_start():
+    pass
+
+def scence_setup():
+    pass
+
+def scene_main():   
+    pass
+
+def scene_battle():
+    pass
+
+def scene_win():
+    pass
+
+def scene_lose():
+    pass
