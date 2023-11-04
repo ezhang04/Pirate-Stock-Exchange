2d875e919649293f5bf8c07aa001743d8717f94a
ezhang <ericzhang200@gmail.com>
2023-11-04 16:01:40 -0400

    added treasure and death function

--- mechanics.py
+++ mechanics.py
@@ -46,3 +46,22 @@ def battle_phase(user_crew, enemy_crew):
     else:
         return 0
     
+def treasure (crew):
+    value = random.randrange(0,501)
+    temp = crew.getWealth()
+    crew.setWealth(temp+value)
+
+def death (crew):
+    death = []
+    death[0] = "Captain Slips-a-Lot fell overboard. Lost 1 crew member"
+    death[1] = "No-Toes Ned the Napper fell out of the crow's nest while naping. Lost 1 crew member"
+    death[2] = "Stumblebum Stan the Stinker died to an infection. Lost 1 crew member"
+    death[3] = "Gigglin' Gus the Gullible accidently shot himself with his flintknock pistol. Lost 1 crew member"
+    death[4] = "Captain Giggles McScurvy hasn't had an orange in a while. Lost 1 crew member from scurvy"
+    death[5] = "Chucklin' Charlie the Clueless wanted to know what fire tasted like. Lost 1 crew member"
+    death[6] = "Jolly Roger the Jokester made Captain Chucklechops laugh so hard he died. Lost 1 crew member"
+    death[7] = "Captain Guffawsalot forced Chuckles the Clumsy walk the plank. Lost 1 crew member"
+    num = random.randrange(0,8)
+    num1 = crew.getCrew()
+    crew.setCrew(num1-1)
+    return death[num]
