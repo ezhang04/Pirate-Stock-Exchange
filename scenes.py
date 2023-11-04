5c24c6a27fc479b32aabfc73d2f29b532b85a18d
Assaf Izhar <assaf.izhar@gmail.com>
2023-11-04 17:53:04 -0400

    Update scenes.py

--- scenes.py
+++ scenes.py
@@ -46,3 +46,26 @@ class scene_handler:
 
     def scene_lose(self):
         pass
+
+class Button:
+    def __init__(self, surface, x, y, width, height, text, color, text_color, action=None):
+        self.surface = surface
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.color = color
+        self.text_color = text_color
+        self.font = pygame.font.Font(None, 36)
+        self.action = action
+
+    def draw(self):
+        pygame.draw.rect(self.surface, self.color, self.rect)
+        text_surface = self.font.render(self.text, True, self.text_color)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        self.surface.blit(text_surface, text_rect)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # Left mouse button
+                if self.rect.collidepoint(event.pos):
+                    if self.action is not None:
+                        self.action()
